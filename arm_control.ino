a431ac3dceff649d69ad8744f57914743f0caa52
szeorlin0711 <seanzeorlin@gmail.com>
2025-04-30 13:17:56 -0500

    Update arm_control.ino
    Added ROS subscription and some motion smoothing. The arm will now play through the saved poses when the detected color is yellow and in_range = true.

--- arm_control.ino
+++ arm_control.ino
@@ -31,44 +31,78 @@ void setup() {
   pinMode(playButtonPin, INPUT_PULLUP);
 
   // Read saved pose count
+  poseCount = EEPROM.read(0);#include <EEPROM.h>
+#include <Servo.h>
+#include <ros.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/String.h>
+
+#define NUM_SERVOS 6
+#define MAX_POSES 10
+#define SMOOTH_STEP 2  // Max degrees per step
+
+const int potPins[NUM_SERVOS] = {A0, A1, A2, A3, A4, A5};
+const int servoPins[NUM_SERVOS] = {2, 3, 4, 5, 6, 7};
+const int saveButtonPin = 8;
+
+Servo servos[NUM_SERVOS];
+int currentPose[NUM_SERVOS];      // live positions (for manual mode)
+int targetPose[NUM_SERVOS];       // target positions (during playback)
+int currentServoAngles[NUM_SERVOS]; // interpolated servo positions
+
+bool isPlaying = false;
+bool inRange = false;
+String detectedColor = "";
+
+int poseCount = 0;
+int playIndex = 0;
+unsigned long lastStepTime = 0;
+const unsigned long stepDelay = 50; // shorter for smoother stepping
+unsigned long lastMoveTime = 0;
+const unsigned long moveInterval = 20; // control smoothing speed
+
+// ROS setup
+ros::NodeHandle nh;
+
+void inRangeCallback(const std_msgs::Bool& msg) {
+  inRange = msg.data;
+}
+
+void colorCallback(const std_msgs::String& msg) {
+  detectedColor = msg.data;
+}
+
+ros::Subscriber<std_msgs::Bool> sub_inRange("/in_range", &inRangeCallback);
+ros::Subscriber<std_msgs::String> sub_detected("/detected_color", &colorCallback);
+
+void setup() {
+  //Serial.begin(57600);
+  nh.initNode();
+  nh.subscribe(sub_inRange);
+  nh.subscribe(sub_detected);
+
+  for (int i = 0; i < NUM_SERVOS; i++) {
+    servos[i].attach(servoPins[i]);
+    currentServoAngles[i] = 90;
+    servos[i].write(currentServoAngles[i]);
+  }
+
+  pinMode(saveButtonPin, INPUT_PULLUP);
+
   poseCount = EEPROM.read(0);
   poseCount = constrain(poseCount, 0, MAX_POSES);
 }
 
 void loop() {
+  nh.spinOnce();
 
-  static bool lastSaveButtonState = HIGH;
-  static bool lastPlayButtonState = HIGH;
-
-  bool currentSaveButtonState = digitalRead(saveButtonPin);
-  bool currentPlayButtonState = digitalRead(playButtonPin);
-
-  if (currentSaveButtonState != lastSaveButtonState){
-    if (currentSaveButtonState == LOW) {
-      Serial.println("Save Button Pressed");
-    } else {
-      Serial.println("Save Button Released");
+  if (inRange && detectedColor == "yellow") {
+    if (!isPlaying) {
+      isPlaying = true;
+      playIndex = 0;
+      lastStepTime = millis();
+      loadPose(playIndex, targetPose);
     }
-    lastSaveButtonState = currentSaveButtonState;
-    delay(50);
-  }
-  
-  if (currentPlayButtonState != lastPlayButtonState) {
-    if (currentPlayButtonState == LOW) {
-      Serial.println("Play Button Pressed");
-    } else {
-      Serial.println("Play Button Released");
-    }
-    lastPlayButtonState = currentPlayButtonState;
-    delay(50);
-  }
-
-  if (digitalRead(playButtonPin) == LOW) {
-    delay(200);
-    isPlaying = !isPlaying;
-    playIndex = 0;
-    lastStepTime = millis();
-    while (digitalRead(playButtonPin) == LOW);  // Wait for release
   }
 
   if (!isPlaying) {
@@ -78,51 +112,57 @@ void loop() {
       int angle = map(val, 0, 1023, 0, 180);
       currentPose[i] = angle;
       servos[i].write(angle);
+      currentServoAngles[i] = angle;  // Sync internal state
     }
 
     if (digitalRead(saveButtonPin) == LOW && poseCount < MAX_POSES) {
       delay(200);
       savePose(poseCount, currentPose);
       poseCount++;
-      EEPROM.write(0, poseCount);  // Update pose count
-      Serial.print("Saved pose "); Serial.println(poseCount);
-      while (digitalRead(saveButtonPin) == LOW);  // Wait for release
+      EEPROM.write(0, poseCount);
+      while (digitalRead(saveButtonPin) == LOW);
     }
   } else {
-    // Play saved poses in loop
-    if (poseCount > 0 && millis() - lastStepTime >= stepDelay) {
-      int pose[NUM_SERVOS];
-      loadPose(playIndex, pose);
+    // Smooth movement
+    if (millis() - lastMoveTime >= moveInterval) {
+      bool allAtTarget = true;
       for (int i = 0; i < NUM_SERVOS; i++) {
-        servos[i].write(pose[i]);
+        int delta = targetPose[i] - currentServoAngles[i];
+        if (abs(delta) > SMOOTH_STEP) {
+          currentServoAngles[i] += (delta > 0) ? SMOOTH_STEP : -SMOOTH_STEP;
+          allAtTarget = false;
+        } else if (delta != 0) {
+          currentServoAngles[i] = targetPose[i]; // snap to target
+        }
+        servos[i].write(currentServoAngles[i]);
       }
-      playIndex = (playIndex + 1) % poseCount;
-      lastStepTime = millis();
+
+      lastMoveTime = millis();
+
+      // When finished current pose
+      if (allAtTarget && millis() - lastStepTime >= stepDelay) {
+        playIndex++;
+        if (playIndex < poseCount) {
+          loadPose(playIndex, targetPose);
+          lastStepTime = millis();
+        } else {
+          isPlaying = false;
+        }
+      }
     }
   }
 }
 
 void savePose(int index, int pose[]) {
   int addr = 1 + index * NUM_SERVOS;
-  Serial.print("Saving pose "); Serial.print(index); Serial.print(" at EEPROM addr "); Serial.println(addr);
   for (int i = 0; i < NUM_SERVOS; i++) {
     EEPROM.write(addr + i, pose[i]);
-    Serial.print("  Servo "); Serial.print(i); Serial.print(" = "); Serial.println(pose[i]);
   }
 }
 
 void loadPose(int index, int pose[]) {
   int addr = 1 + index * NUM_SERVOS;
-  Serial.print("Loading pose "); Serial.print(index); Serial.print(" from EEPROM addr "); Serial.println(addr);
   for (int i = 0; i < NUM_SERVOS; i++) {
     pose[i] = EEPROM.read(addr + i);
-    Serial.print("  Servo "); Serial.print(i); Serial.print(" = "); Serial.println(pose[i]);
   }
-}
-
-void clearEEPROM() {
-  for (int i = 0; i < EEPROM.length(); i++) {
-    EEPROM.write(i, 0);
-  }
-  Serial.println("EEPROM cleared!");
 }
